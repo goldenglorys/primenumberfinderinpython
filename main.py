e5095734d81b9558650591606d5e6dedeb0948e7
Olusola Glory <olusolagloryolamide@gmail.com>
2020-08-11 14:00:35 +0100

    Wrote a python program that finds all prime numbers between a & b and writes them into a file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def prime_test(n):
     if n < 2:
         return False
     for number in islice(count(2), int(sqrt(n)-1)):
-        if not n%number:
+        if not n % number:
             return False
     return True
 
